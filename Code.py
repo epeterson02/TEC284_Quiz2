fbfb71d609060e2b280f5a54f4f4f78417303603
epeterson02 <157244412+epeterson02@users.noreply.github.com>
2025-04-10 14:22:49 -0500

    Update Code.py to include defined method
    Added method for taking button input and turning LEDs on

--- Code.py
+++ Code.py
@@ -8,3 +8,22 @@ blue_led = LED(21)
 red_button = Button(20)
 green_button = Button(6)
 blue_button = Button(26)
+
+def check_buttons():
+    if red_button.is_pressed:
+        print("Red is pressed")
+        red_led.on()
+    else:
+        red_led.off()
+
+    if green_button.is_pressed:
+        print("Green is pressed")
+        green_led.on()
+    else:
+        green_led.off()
+
+    if blue_button.is_pressed:
+        print("Blue is pressed")
+        blue_led.on()
+    else:
+        blue_led.off()
